ee36498e69d1864a498becc475c28a3b558a5c69
Liamm02 <90764145+Liamm02@users.noreply.github.com>
2022-10-11 09:02:04 +0100

    Add files via upload

---
+++ Inverted_index/Document_normalizer.py
@@ -0,0 +1,36 @@
+import string
+from nltk.corpus import stopwords
+
+
+def Translator(file):
+    """This function gets rid of all the punctuations and special characters,
+    it also converts the document to lowercase"""
+    translator = str.maketrans(dict.fromkeys(string.punctuation, ' '))
+
+    # Convert the entire document to lower case to not have duplicate words
+    f = file.translate(translator).lower()
+    special = ["×", "·", "”", "“", "—", "«", "»", "—", "º", "ª", "π", '~', ':', "'", '+', '[', '\\', '@', '^', '{', '%',
+               '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.', '_', '=', ']', '!', '>', ';', '?', '¿', '¡', '#',
+               '$', ')', '/']
+
+    for character in special:
+        f = f.replace(character, "")
+    return f
+
+def StopWords_deleter(text):
+    """This function gets rid of the stopwords and stores those words that aren't into a dictionary"""
+    stopWords = set(stopwords.words('spanish')) | set(stopwords.words('english'))
+
+    # Now we create the dictionary that stores all the words and a list with the lines it appears in
+    d = {}
+    for num, line in enumerate(text):
+        for word in line.split(" "):
+            if word != "":
+                if word in stopWords:
+                    continue
+                else:
+                    if word in d:
+                        d[word].append(num + 1)
+                    if word not in d:
+                        d[word] = [num + 1]
+    return d
---
+++ Inverted_index/main.py
@@ -0,0 +1,27 @@
+import os
+from Inverted_Index import inverted_index_of
+from Checker import Checker
+
+
+"""If you want to change where the Inverted index is stored, change MainPath.
+Documents must be stored in the Documents folder in said path"""
+
+MainPath = "./"
+
+"""We check if every file and folder we need is ready, and prepare them if they are not"""
+Check = Checker(MainPath)
+documents = Check.documents_checker()
+Check.IE_File_checker()
+Check.Indices_Folder_checker()
+
+
+"""Now we create the inverted index with al documents in the Document folder"""
+for doc in documents:
+    if not os.path.isfile(MainPath+"indices/"+doc[0:-3]+"json"):
+        print("Inverting and adding:        ",doc[0:-4])
+        inverted_index_of(doc, MainPath)
+    else:
+        print("Already inverted, updating:  ",doc[0:-4])
+        inverted_index_of(doc, MainPath, 1)
+
+
